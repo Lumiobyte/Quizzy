08bbc0425367198dd921e900612b0b89657b1c4a
Dehydrated <lucas.a.mckee@gmail.com>
2025-08-23 14:10:30 +1000

    changes made

---
+++ Quizzy/Quizzy/Controllers/QuizCreatorController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Quizzy.Models;
+
+namespace Quizzy.Controllers
+{
+    public class QuizCreatorController : Controller
+    {
+        public bool exists = true;
+    }
+}
