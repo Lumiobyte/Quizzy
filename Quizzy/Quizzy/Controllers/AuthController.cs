d352d547e537c5d4b928e22d7ad92658e0f41c10
Dehydrated <lucas.a.mckee@gmail.com>
2025-08-25 14:29:20 +1000

    Main coding completed outside of validation

---
+++ Quizzy/Quizzy/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Quizzy.Models;
+
+namespace Quizzy.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            // Get user from database
+            // Return id
+            int id = 1;
+
+            return Ok(new {id});
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterRequest request)
+        {
+            // Validate creds
+            // Create user
+            int? id = null;
+
+            return Ok(new { id, message="failed!!!" });
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
