bb485437cf3640fc2a140d37bd2f12ee5a59e777
Lumiobyte <evanpartridge0@gmail.com>
2025-08-25 11:44:09 +1000

    Implement UOW and generic repository interface

---
+++ Quizzy/Quizzy.Core/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzy.Core.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly QuizzyDbContext _dbContext;
+
+        public UnitOfWork(QuizzyDbContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // If there is any code we would like to always run before writing to the DB, we can call those functions here!
+            // E.g. some kind of validation, logging, counters etc. If you not sure if this is the right place for it, ask Evan
+
+            return _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
