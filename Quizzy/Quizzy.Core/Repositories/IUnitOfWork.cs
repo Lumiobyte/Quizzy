373b41639d9ec067124885ee1ba3eb9feb41758d
Lumiobyte <evanpartridge0@gmail.com>
2025-08-27 00:32:39 +1000

    Setup DI for database

--- Quizzy/Quizzy.Core/Repositories/IUnitOfWork.cs
+++ Quizzy/Quizzy.Core/Repositories/IUnitOfWork.cs
@@ -9,6 +9,14 @@ namespace Quizzy.Core.Repositories
     public interface IUnitOfWork
     {
 
+        public IQuizRepository Quizzes { get; }
+        public IQuizQuestionRepository QuizQuestions { get; }
+        public IQuizAnswerRepository QuizAnswers { get; }
+        public IQuizPlayerRepository QuizPlayers { get; }
+        public IQuizSessionRepository QuizSessions { get; }
+        public IPlayerAnswerRepository PlayerAnswers { get; }
+        public IUserAccountRepository UserAccounts { get; }
+
         public Task SaveChangesAsync(CancellationToken cancellationToken = default);
 
     }
--- Quizzy/Quizzy.Core/Repositories/UnitOfWork.cs
+++ Quizzy/Quizzy.Core/Repositories/UnitOfWork.cs
@@ -11,8 +11,24 @@ namespace Quizzy.Core.Repositories
 
         private readonly QuizzyDbContext _dbContext;
 
+        public IQuizRepository Quizzes { get; }
+        public IQuizQuestionRepository QuizQuestions { get; }
+        public IQuizAnswerRepository QuizAnswers { get; }
+        public IQuizPlayerRepository QuizPlayers { get; }
+        public IQuizSessionRepository QuizSessions { get; }
+        public IPlayerAnswerRepository PlayerAnswers { get; }
+        public IUserAccountRepository UserAccounts { get; }
+
         public UnitOfWork(QuizzyDbContext dbContext) { 
             _dbContext = dbContext;
+
+            Quizzes = new QuizRepository(dbContext);
+            QuizQuestions = new QuizQuestionRepository(dbContext);
+            QuizAnswers = new QuizAnswerRepository(dbContext);
+            QuizPlayers = new QuizPlayerRepository(dbContext);
+            QuizSessions = new QuizSessionRepository(dbContext);
+            PlayerAnswers = new PlayerAnswerRepository(dbContext);
+            UserAccounts = new UserAccountRepository(dbContext);
         }
 
         public Task SaveChangesAsync(CancellationToken cancellationToken = default)
