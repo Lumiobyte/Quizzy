1021639131aed2936f3bbfd8a9b2652b5b49de9e
Lumiobyte <evanpartridge0@gmail.com>
2025-08-31 12:52:45 +1000

    Fix interface

--- Quizzy/Quizzy.Core/Repositories/IQuizSessionRepository.cs
+++ Quizzy/Quizzy.Core/Repositories/IQuizSessionRepository.cs
@@ -4,5 +4,7 @@ namespace Quizzy.Core.Repositories
 {
     public interface IQuizSessionRepository : IRepository<QuizSession>
     {
+        Task<QuizSession?> GetByIdWithDetailsAsync(Guid id);
+        Task<QuizQuestion?> GetNextQuestion(QuizSession session, int currentOrderIndex);
     }
 }
